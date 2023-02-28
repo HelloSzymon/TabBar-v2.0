955c78d6d185f2c5c7d948b4dedb42cba75c67ff
Szymon Wnuk <101000022+HelloSzymon@users.noreply.github.com>
2023-02-28 10:29:30 +0100

    Initial Commit

---
+++ TabBar v2.0/TabBar_v2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar_v2_0App.swift
+//  TabBar v2.0
+//
+//  Created by Szymon Wnuk on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabBar_v2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
